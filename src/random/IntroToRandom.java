0a4c26bc8d2d042954f862a6296bfa1365de1acc
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-01-23 19:25:13 -0800

    1.DiceRoll.class-bin/random 2.DiceRoll.java-src/random 3.IntroToRandom.class-bin/random 4.IntroToRandom.java-src/random 5.RockPaperScissors.class-bin/random 6.RockPaperScissors-java-src/random

--- src/random/IntroToRandom.java
+++ src/random/IntroToRandom.java
@@ -8,17 +8,20 @@ import java.util.Random;
 //1. Run the program.  What number appears in the console?
 //   Run it again. Is the number the same?
 
-
 public class IntroToRandom {
 	public static void main(String[] args) {
 		int r = 0;
-		
+		Random randy=new Random();
+				 r =randy.nextInt((75-25)+1)+25;
+				 r=randy.nextInt((88+222)+1)+-222;
+
 		//2. Now make r random. 
 		//   Create an object of the Random class
 		//   Hint: new Random().nextInt()
 
+				 
 		//3. Limit the random number between 0 and 100
-
+	 
 		//4. Limit the random number between 25 and 75. Hint: (highest value - lowest value) + lowest value
 
 		//5. Challenge: Limit the random number between -222 and 88
